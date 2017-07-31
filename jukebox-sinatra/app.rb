823d3277b8d3e7e1f88a2e582eddc4c9bf0c6a22
Johanna Wagner <johanna.wagner@whu.edu>
2017-07-31 11:48:31 -0300

    Generated a new project within lewagon/middleman-template

---
+++ jukebox-sinatra/app.rb
@@ -0,0 +1,10 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "sqlite3"
+
+DB = SQLite3::Database.new(File.join(File.dirname(__FILE__), 'db/jukebox.sqlite'))
+
+get "/" do
+  # TODO: Gather all artists to be displayed on home page
+  erb :home # Will render views/home.erb file (embedded in layout.erb)
+end
